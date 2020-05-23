0cb51ba9b89d058198d7cb2d2a371fb7d2d919b9
Sabbir <sabbir106706@gmail.com>
2020-05-24 04:11:21 +0600

    p1 using cpp

---
+++ Beginners Problems/1000 - Greetings from LightOJ/1000 - Greetings from LightOJ.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int T;
+    cin>>T;
+   for(int i=1;i<=T;i++)
+    {
+    	int a,b;
+    	cin>>a>>b;
+    	cout<<"Case "<<i<<": "<<a+b<<"\n" ;
+    }
+    return 0;
+}
+
